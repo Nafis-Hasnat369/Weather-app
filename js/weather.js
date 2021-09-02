c67c348cd4bdd511bd5bcc6e6ef6462cd2b09741
Nafis-Hasnat369 <nafis7823@gmail.com>
2021-09-02 22:49:53 +0600

    codes are organized

--- js/weather.js
+++ js/weather.js
@@ -3,28 +3,27 @@ const searchCity = async () => {
     const unit = "metric";
     const inputCity = document.getElementById("input-city");
     const cityName = inputCity.value;
+
     // clear values
-    // clearData();
     inputCity.value = "";
+
     // const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=${unit}&appid=${apiKey}`;
     const res = await fetch(url);
     const data = await res.json();
-    console.log(data);
     displayData(data);
 };
 const getTheIcon = async (data) => {
     const img = document.getElementById("weather-img");
-    img.innerHTML = "";
     const icon = data.weather[0].icon;
     img.innerHTML = `<img id="weather-img" style="width:100px; height:100px" src="https://openweathermap.org/img/wn/${icon}.png" alt="">`;
 };
+const setInnerText = (id, text) => {
+    document.getElementById(id).innerText = text;
+}
 const displayData = (data) => {
     getTheIcon(data);
-    const city = document.getElementById("city");
-    const temp = document.getElementById("temp");
-    const weather = document.getElementById("weather");
-    city.innerText = data.name;
-    temp.innerText = data.main.temp;
-    weather.innerText = data.weather[0].main;
+    setInnerText("city", data.name);
+    setInnerText("temp", data.main.temp);
+    setInnerText("weather", data.weather[0].main);
 };
